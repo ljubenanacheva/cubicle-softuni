a96c09a1a20f2bc0719c7a0eff03f0d60efea567
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-02 16:22:52 +0200

    Setup static assets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const config=require('./config.js');
+const handlebars=require('express-handlebars');
 
 const app=express();
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine','hbs');
+app.set('views','./src/views');
+
 app.get('/',(req,res)=>{
-    res.send('Home page');
+    res.render('home');
 });
 
 app.listen(config.PORT,()=>console.log(`Server is running on port ${config.PORT}...`));
