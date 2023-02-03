dd2ca8345dff9321378d631873fa6356b8e28bb9
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-03 22:04:01 +0200

    Add details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube=require('../models/Cube.js');
+const db=require('../db.json');
 
 exports.getCreateCube=(req,res)=>{
     res.render('create');
@@ -10,3 +11,16 @@ exports.postCreateCube=(req,res)=>{
     Cube.save(cube);
     res.redirect('/');
 };
+
+exports.getDetails=(req,res)=>{
+    let cubeId=Number(req.params.cubeId);
+    if(!cubeId){
+        return res.redirect('404');
+    }
+    let cube=db.cubes.find(x=>x.id===cubeId);
+
+    if(!cube){
+        return res.redirect('404');
+    }
+    res.render('details',{cube})
+}
