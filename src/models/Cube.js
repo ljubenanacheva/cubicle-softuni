3d2868eb667239c6194a51415126cc4794b7a05a
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-06 20:49:08 +0200

    Add Cube model

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,21 +1,27 @@
-const db=require('../db.json');
-const fs=require('fs');
-const path=require('path');
+const {Schema,model}=require('mongoose');
 
-class Cube{
-    constructor(name,description,imageUrl,difficultyLevel){
-        this.name=name,
-        this.description=description,
-        this.imageUrl=imageUrl,
-        this.difficultyLevel=difficultyLevel
+const cubeSchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+        maxLength:50,
+    },
+    imageUrl:{
+        type:String,
+        required:true,
+    },
+    difficultyLevel:{
+        type:Number,
+        required:true,
+        max:6,
+        min:1,
     }
+});
 
-    save(){
-        this.id = db.cubes[db.cubes.length - 1].id + 1;
-        db.cubes.push(this);
-        const jsonData=JSON.stringify(db,null, 2);
-        fs.writeFileSync(path.resolve(__dirname,'../db.json'),jsonData);
-    }
-}
+const Cube=model('Cube',cubeSchema);
 
 module.exports=Cube;
