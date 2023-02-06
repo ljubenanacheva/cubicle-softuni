f657d40836f4284fa64c62451683798fa9c0e461
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-06 23:47:51 +0200

    Show cubes in Home Page from DB.

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,6 +3,10 @@ const handlebars=require('express-handlebars');
 function setupViewEngine(app){
     app.engine('hbs', handlebars.engine({
         extname: 'hbs',
+        runtimeOptions: {
+                  allowProtoPropertiesByDefault: true,
+                  allowProtoMethodsByDefault: true,
+                },
     }));
     app.set('view engine','hbs');
     app.set('views','./src/views');
