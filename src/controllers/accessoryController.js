fea7d23bf10a3fa4eb67f132c92c36eb21324520
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-07 00:46:22 +0200

    Add accessory to DB.

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,14 @@
 const router=require('express').Router();
+const Accessory=require('../models/Accessory.js');
 
 router.get('/create', (req,res)=>{
     res.render('accessory/create');
+});
+
+router.post('/create',async (req,res)=>{
+    const {name, description,imageUrl}=req.body;
+    await Accessory.create({name, description,imageUrl});
+    res.redirect('/');
 });
 
 module.exports=router;
