ec90252ac8212f85314bf25950eb4fceaeecad74
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-07 00:20:40 +0200

    Added accessory model.

---
+++ src/models/Accessory.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+
+const accessoryShcema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    imageUrl:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+        maxLength:50
+    }
+});
+
+const Accessory=mongoose.model('Accessory',accessoryShcema);
+
+module.exports=Accessory;
