54c8d9e66c59c6a3a3c2a4461bef0b2866b4ad95
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-08 00:25:31 +0200

    Add login view.

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ const router=require('express').Router();
 const cubeController=require('./controllers/cubeController.js');
 const homeController=require('./controllers/homeController.js');
 const accessoryController=require('./controllers/accessoryController.js');
+const authController=require('./controllers/authController.js');
 
 
 router.get('/',homeController.getHomePage);
 router.get('/about',homeController.getAboutPage);
 router.get('/404',homeController.getErrorPage);
+
+router.use('/',authController);
 
 router.get('/cubes/create',cubeController.getCreateCube);
 router.post('/cubes/create',cubeController.postCreateCube);
