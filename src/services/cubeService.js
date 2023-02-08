66e55dbb4baf357e91927951113b488a0d457943
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-09 00:48:48 +0200

    Add edit cube functionality.

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,3 +1,5 @@
 const Cube=require('../models/Cube.js');
 
 exports.getOne=(cubeId)=>Cube.findById(cubeId);
+
+exports.update=(cubeId,data)=>Cube.findByIdAndUpdate(cubeId,data,{runValidators:true});
