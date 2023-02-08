1b82990ffd992bbb2cef75a10d8367bab882ef40
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-02-08 21:44:56 +0200

    Updated auth middleware.

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -7,12 +7,19 @@ exports.authentication=async(req,res,next)=>{
         try{
         const decodedToken=await jwt.verify(token,config.SECRET);
         req.user=decodedToken;
-        
+        req.isAuthenticated=true;
         }catch(err){
         console.log(err);
         res.clearCookie('auth');
         return res.redirect('/404');
         }
+    }
+    next();
+}
+
+exports.isAuthenticated=(req,res,next)=>{
+    if(!req.isAuthenticated){
+        return res.redirect('/login');
     }
     next();
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const cubeController=require('./controllers/cubeController.js');
 const homeController=require('./controllers/homeController.js');
 const accessoryController=require('./controllers/accessoryController.js');
 const authController=require('./controllers/authController.js');
+const {isAuthenticated}=require('./middlewares/authMiddleware.js');
 
 
 router.get('/',homeController.getHomePage);
@@ -12,8 +13,8 @@ router.get('/404',homeController.getErrorPage);
 
 router.use('/',authController);
 
-router.get('/cubes/create',cubeController.getCreateCube);
-router.post('/cubes/create',cubeController.postCreateCube);
+router.get('/cubes/create',isAuthenticated, cubeController.getCreateCube);
+router.post('/cubes/create',isAuthenticated, cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details',cubeController.getDetails);
 
 router.get('/cubes/:cubeId/attach',cubeController.getAttachAccessory);
